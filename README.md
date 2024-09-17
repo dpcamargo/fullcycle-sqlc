9f9761b29ddcc51d34c2dd285280ca270910e260
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-09-16 21:42:05 -0300

    CRUD

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-1 - Create migration ```migrate create -ext=sql -dir=sql/migrations -seq init```
+1 - Create migration `migrate create -ext=sql -dir=sql/migrations -seq init`
 
 2 - Create UP and DOWN migrations in ./sql/migrations
 
-3 - Up migration ```migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3306)/courses" -verbose up```
+3 - Up migration `migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3306)/courses" -verbose up`
 
-4 - Down migration ```migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3306)/courses" -verbose down```
+4 - Down migration `migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3306)/courses" -verbose down`
 
-5 - Install SQLc ```brew install sqlc```
+5 - Install SQLc `brew install sqlc`
+
+6 - Edit sqlc.yaml
+
+```version: "2"
+sql:
+  - schema: "sql/migrations"
+    queries: "sql/queries"
+    engine: "mysql"
+    gen:
+      go:
+        package: "db"
+        out: "internal/db"
+```
+
+7 - Generate code ```sqlc generate````
--- cmd/runSQLC/main.go
+++ cmd/runSQLC/main.go
@@ -1,5 +1,74 @@
 package main
 
-func main() {
+import (
+	"context"
+	"database/sql"
+	"fmt"
 
+	"github.com/dpcamargo/fullcycle-sqlc/internal/db"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/courses")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	queries := db.New(dbConn)
+
+	err = queries.CreateCategory(ctx, db.CreateCategoryParams{
+		ID:          uuid.New().String(),
+		Name:        "Name",
+		Description: sql.NullString{String: "Description", Valid: true},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	categories, err := queries.ListCategories(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("--- Before Update ---")
+	for _, category := range categories {
+		fmt.Println(category.ID, category.Name, category.Description.String)
+	}
+
+	err = queries.UpdateCategory(ctx, db.UpdateCategoryParams{
+		ID:          categories[0].ID,
+		Name:        "New Name",
+		Description: sql.NullString{String: "New Description", Valid: true},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	categories, err = queries.ListCategories(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("--- After Update ---")
+	for _, category := range categories {
+		fmt.Println(category.ID, category.Name, category.Description.String)
+	}
+
+	err = queries.DeleteCategory(ctx, categories[0].ID)
+	if err != nil {
+		panic(err)
+	}
+
+	categories, err = queries.ListCategories(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("--- After Delete ---")
+	for _, category := range categories {
+		fmt.Println(category.ID, category.Name, category.Description.String)
+	}
 }
