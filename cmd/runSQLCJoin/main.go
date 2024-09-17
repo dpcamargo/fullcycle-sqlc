0346b81989b37d8b7373af6591d330ee4f4e0e1c
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-09-16 23:17:00 -0300

    Joins

---
+++ cmd/runSQLCJoin/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/dpcamargo/fullcycle-sqlc/internal/db"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type CourseDB struct {
+	dbConn *sql.DB
+	*db.Queries
+}
+
+type CourseParams struct {
+	ID          string
+	Name        string
+	Description sql.NullString
+	Price       float64
+}
+
+type CategoryParams struct {
+	ID          string
+	Name        string
+	Description sql.NullString
+}
+
+func NewCourseDB(dbConn *sql.DB) *CourseDB {
+	return &CourseDB{
+		dbConn:  dbConn,
+		Queries: db.New(dbConn),
+	}
+}
+
+func (c *CourseDB) callTx(ctx context.Context, fn func(*db.Queries) error) error {
+	tx, err := c.dbConn.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := db.New(tx)
+	err = fn(q)
+	if err != nil {
+		if errRb := tx.Rollback(); errRb != nil {
+			return fmt.Errorf("error on rollback: %v, original error: %w", errRb, err)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+func (c *CourseDB) CreateCourseAndCategory(ctx context.Context, argsCategory CategoryParams, argsCourse CourseParams) error {
+	err := c.callTx(ctx, func(q *db.Queries) error {
+		var err error
+		err = q.CreateCategory(ctx, db.CreateCategoryParams{
+			ID:          argsCategory.ID,
+			Name:        argsCategory.Name,
+			Description: argsCategory.Description,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = q.CreateCourse(ctx, db.CreateCourseParams{
+			ID:          argsCourse.ID,
+			Name:        argsCourse.Name,
+			Description: argsCourse.Description,
+			Price:       argsCourse.Price,
+			CategoryID:  argsCategory.ID,
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/courses")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	queries := db.New(dbConn)
+
+	courses, err := queries.ListCourses(ctx)
+	if err != nil {
+		panic(err)
+	}
+	for _, course := range courses {
+		fmt.Printf("Category: %s, Course ID: %s, Course Name: %s, Course Description: %s, Course Price: %f\n",
+			course.CategoryName, course.ID, course.Name, course.Description.String, course.Price)
+	}
+
+}
--- sql/queries/query.sql
+++ sql/queries/query.sql
@@ -20,3 +20,7 @@ WHERE id = ?;
 -- name: CreateCourse :exec
 INSERT INTO courses (id, name, description, price, category_id)
 VALUES (?, ?, ?, ?, ?);
+
+-- name: ListCourses :many
+SELECT c.*, ca.name as category_name
+FROM courses c JOIN categories ca ON c.category_id = ca.id;
